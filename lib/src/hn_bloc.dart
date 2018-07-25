50469c7928651a32afa4c73a8705b1a8acbd9848
Filip Hracek <filip.hracek@gmail.com>
2018-07-25 14:30:01 -0700

    Add isLoading stream

--- lib/src/hn_bloc.dart
+++ lib/src/hn_bloc.dart
@@ -22,6 +22,10 @@ class HackerNewsBloc {
     17387851,
   ];
 
+  Stream<bool> get isLoading => _isLoadingSubject.stream;
+
+  final _isLoadingSubject = BehaviorSubject<bool>(seedValue: false);
+
   final _articlesSubject = BehaviorSubject<UnmodifiableListView<Article>>();
 
   var _articles = <Article>[];
@@ -52,10 +56,11 @@ class HackerNewsBloc {
     }
   }
 
-  _getArticlesAndUpdate(List<int> ids) {
-    _updateArticles(ids).then((_) {
-      _articlesSubject.add(UnmodifiableListView(_articles));
-    });
+  _getArticlesAndUpdate(List<int> ids) async {
+    _isLoadingSubject.add(true);
+    await _updateArticles(ids);
+    _articlesSubject.add(UnmodifiableListView(_articles));
+    _isLoadingSubject.add(false);
   }
 
   Future<Null> _updateArticles(List<int> articleIds) async {
