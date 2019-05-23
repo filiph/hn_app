78abfb46c37e2db3b07461779b8c716b4912f831
Filip Hracek <filip.hracek@gmail.com>
2019-05-23 16:42:42 -0700

    Move files around
    Renaming and moving files in order to reflect the current structure better.

--- lib/src/widgets/loading_info.dart
+++ lib/src/widgets/loading_info.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:hn_app/src/hn_bloc.dart';
+import 'package:hn_app/src/notifiers/hn_api.dart';
 import 'package:provider/provider.dart';
 
 class LoadingInfo extends StatefulWidget {
